7e26675a0d84840a28cf3b4ee702f713d1d7d9d2
Kaan EFE <kaefean1@gmail.com>
2024-12-10 18:56:49 +0300

    sapi counter dialog baseline create

--- sapiensshifter/lib/main.dart
+++ sapiensshifter/lib/main.dart
@@ -2,6 +2,9 @@
 
 import 'package:flutter/material.dart';
 import 'package:sapiensshifter/feature/theme/appliaction_theme.dart';
+import 'package:sapiensshifter/product/component/preview_table_card.dart';
+import 'package:sapiensshifter/product/component/sapi_counter_dialog.dart';
+import 'package:sapiensshifter/product/export_dependency_package/component_export_package.dart';
 import 'package:sizer/sizer.dart';
 
 void main() {
@@ -29,6 +32,24 @@ class Thix extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          context.popupManager.showLoader(
+            barrierDismissible: true,
+            widgetBuilder: (context) => Dialog(
+              shape: RoundedRectangleBorder(
+                  borderRadius: context.border.normalBorderRadius),
+              child: SapiCounterDialog(
+                titleName: 'Masa 12',
+                buttonText: 'Onayla',
+                onPressed: (val) {
+                  debugPrint('$val');
+                },
+              ),
+            ),
+          );
+        },
+      ),
       body: Center(
         child: Padding(
           padding: const EdgeInsets.all(8),
