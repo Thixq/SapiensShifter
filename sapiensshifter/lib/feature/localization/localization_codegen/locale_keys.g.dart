43128a57acddc929a660f0a802abdb5c4e28310a
Kaan EFE <kaefean1@gmail.com>
2024-12-23 23:16:28 +0300

    localization baseline created

---
+++ sapiensshifter/lib/feature/localization/localization_codegen/locale_keys.g.dart
@@ -0,0 +1,10 @@
+// DO NOT EDIT. This is code generated via package:easy_localization/generate.dart
+
+abstract class  LocaleKeys {
+  static const price_symbol = 'price_symbol';
+  static const confirm = 'confirm';
+  static const drop_down_drop_down_default = 'drop_down.drop_down_default';
+  static const drop_down_drop_down_extra = 'drop_down.drop_down_extra';
+  static const drop_down = 'drop_down';
+
+}
--- sapiensshifter/lib/main.dart
+++ sapiensshifter/lib/main.dart
@@ -1,10 +1,29 @@
 // ignore_for_file: prefer_const_constructors
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:sapiensshifter/feature/localization/localization.dart';
 import 'package:sapiensshifter/feature/theme/appliaction_theme.dart';
+import 'package:sapiensshifter/product/component/custom_avatar.dart';
+import 'package:sapiensshifter/product/component/custom_nav_bar.dart';
+import 'package:sapiensshifter/product/component/message_info_list_tile.dart';
+import 'package:sapiensshifter/product/component/preview_product_card.dart';
+import 'package:sapiensshifter/product/component/preview_table_card.dart';
+import 'package:sapiensshifter/product/component/sapi_button.dart';
+import 'package:sapiensshifter/product/component/sapi_custom_drop_down.dart';
+import 'package:sapiensshifter/product/component/sapi_text_field.dart';
+import 'package:sapiensshifter/product/component/week_card.dart';
 import 'package:sapiensshifter/product/utils/export_dependency_package/component_export_package.dart';
+import 'package:sapiensshifter/product/utils/export_dependency_package/nav_bar_export.dart';
+import 'package:sapiensshifter/product/utils/export_dependency_package/table_export.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
+  runApp(
+    LanguageManager(
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -13,8 +32,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Sizer(
-      builder: (context, orientation, screenType) => MaterialApp(
+      builder: (_, __, ___) => MaterialApp(
         theme: SapiensTheme.instance.lightTheme,
+        supportedLocales: context.supportedLocales,
+        localizationsDelegates: context.localizationDelegates,
+        locale: context.locale,
         home: Thix(),
       ),
     );
@@ -26,6 +48,44 @@ class Thix extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: ListView(
+        children: [
+          SapiButton(
+            buttonText: LocaleKeys.confirm.tr(),
+            onPressed: () {},
+          ),
+          SapiTextField(),
+          CustomCircleAvatar(imageUrl: ''),
+          CustomNavBar(items: [NavBarItem(icon: Icons.abc)]),
+          MessageInfoListTile(
+            imageUrl: '',
+            onPressed: () {},
+          ),
+          PreviewProductCard(
+            onPressed: (productId) {},
+            imageUrl: 'imageUrl',
+            productName: 'productName',
+            productId: 'productId',
+            price: 1,
+          ),
+          PreviewTableCard(dataModel: TableModel()),
+          SapiCustomDropDown<String>(
+            items: const {'Kaan': 'Kaan', 'Hey': 'Hat'},
+            isMulti: true,
+            onSelected: (select) {},
+          ),
+          Row(
+            children: [
+              WeekCard(),
+              SizedBox(
+                width: 16,
+              ),
+              WeekCard(),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
