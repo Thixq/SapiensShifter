679a8a172de9ae54fb255c45d9c201dafcedcf49
Kaan EFE <kaefean1@gmail.com>
2024-12-27 23:12:53 +0300

    fix

--- sapiensshifter/lib/product/component/preview_table_card.dart
+++ sapiensshifter/lib/product/component/preview_table_card.dart
@@ -14,23 +14,21 @@ final class PreviewTableCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Ink(
+    return _buildContainer(context);
+  }
+
+  Widget _buildContainer(BuildContext context) {
+    return Container(
       decoration: _buildCardDecoration(context),
+      padding: context.padding.normal,
       child: InkWell(
         onTap: () => onPressed?.call(tableModel),
         borderRadius: context.border.normalBorderRadius,
         child: AspectRatio(
           aspectRatio: 1,
-          child: _buildContainer(context),
+          child: _buildContent(context),
         ),
       ),
-    );
-  }
-
-  Widget _buildContainer(BuildContext context) {
-    return Container(
-      padding: context.padding.normal,
-      child: _buildContent(context),
     );
   }
 
