d5716b10d815df34447ca9a53f1ef4bc0dbd9dc9
Kaan EFE <kaefean1@gmail.com>
2024-12-29 18:25:36 +0300

    preview order card add dismissible

--- sapiensshifter/lib/main.dart
+++ sapiensshifter/lib/main.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:sapiensshifter/feature/localization/localization.dart';
 import 'package:sapiensshifter/feature/theme/appliaction_theme.dart';
+import 'package:sapiensshifter/product/component/custom_avatar.dart';
+import 'package:sapiensshifter/product/component/message_info_list_tile.dart';
+import 'package:sapiensshifter/product/component/order_card.dart';
 import 'package:sapiensshifter/product/component/preview_order_card.dart';
+import 'package:sapiensshifter/product/component/preview_product_card.dart';
+import 'package:sapiensshifter/product/component/preview_table_card.dart';
+import 'package:sapiensshifter/product/component/sapi_button.dart';
+import 'package:sapiensshifter/product/component/sapi_custom_drop_down.dart';
+import 'package:sapiensshifter/product/component/sapi_text_field.dart';
+import 'package:sapiensshifter/product/component/week_card.dart';
 import 'package:sapiensshifter/product/models/order_model.dart';
 import 'package:sapiensshifter/product/models/table_model.dart';
 import 'package:sapiensshifter/product/utils/export_dependency_package/component_export_package.dart';
+import 'package:sapiensshifter/product/utils/export_dependency_package/nav_bar_export.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -39,25 +49,75 @@ class Thix extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(8),
-              child: PreviewOrderCard(
-                tableModel: TableModel(
-                  tableName: 'Masa 12',
-                  orderList: [
-                    OrderModel(price: 12.99),
-                    OrderModel(price: 11),
-                    OrderModel(),
+      body: Stack(
+        children: [
+          Container(),
+          Padding(
+            padding: const EdgeInsets.all(16),
+            child: ListView(
+              children: [
+                CustomCircleAvatar(
+                  imageUrl: ''.ext.randomImage,
+                ),
+                MessageInfoListTile(
+                  onPressed: () {},
+                  imageUrl: '',
+                  title: 'Hayalet Sevgilim',
+                  subTitle: 'Sevmesen beni özledimse seni',
+                ),
+                const OrderCard(),
+                PreviewOrderCard(
+                  tableModel: TableModel(
+                    orderList: [
+                      OrderModel(price: 12),
+                      OrderModel(price: 12),
+                      OrderModel(price: 12),
+                    ],
+                  ),
+                ),
+                const Row(
+                  children: [
+                    WeekCard(),
+                    SizedBox(
+                      width: 8,
+                    ),
+                    WeekCard(),
                   ],
                 ),
+                PreviewProductCard(
+                  onPressed: (productId) {},
+                  imageUrl: 'https://coffee.alexflipnote.dev/random',
+                  productName: 'productName',
+                  productId: 'productId',
+                  price: 2,
+                ),
+                PreviewTableCard(tableModel: TableModel()),
+                SapiButton(
+                  buttonText: 'buttonText',
+                  onPressed: () {},
+                ),
+                const SapiTextField(),
+                SapiCustomDropDown(
+                  items: const {},
+                  onSelected: (select) {},
+                ),
+              ],
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Padding(
+              padding: EdgeInsets.only(
+                bottom: context.general.mediaQuery.padding.bottom,
+              ),
+              child: CustomNavBar(
+                items: [
+                  NavBarItem(icon: Icons.table_bar),
+                ],
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
