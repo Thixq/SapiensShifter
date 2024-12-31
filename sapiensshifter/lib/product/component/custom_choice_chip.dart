1cd88ffec62b8b3936b1946b52f9619321081a61
Kaan EFE <kaefean1@gmail.com>
2024-12-31 16:56:37 +0300

    Some rearrangements of the selection chip list are not finished yet.

---
+++ sapiensshifter/lib/product/component/custom_choice_chip.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:sapiensshifter/product/utils/export_dependency_package/component_export_package.dart';
+
+class CustomChoiceChip<T> extends StatelessWidget {
+  const CustomChoiceChip({
+    required this.isSelected,
+    required this.onSelected,
+    required this.titleAndValue,
+    super.key,
+  });
+  final MapEntry<String, T> titleAndValue;
+  final bool isSelected;
+  final ValueChanged<MapEntry<String, T>> onSelected;
+
+  @override
+  Widget build(BuildContext context) {
+    return ChoiceChip(
+      showCheckmark: false,
+      shape: RoundedRectangleBorder(
+        borderRadius: context.border.highBorderRadius,
+      ),
+      side: BorderSide.none,
+      backgroundColor: Colors.white,
+      label: Text(titleAndValue.key),
+      selected: isSelected,
+      onSelected: (bool selected) {
+        onSelected(titleAndValue);
+      },
+    );
+  }
+}
