8f3bb24a278085b29fa9db06a21a97f3a6edae42
Kaan EFE <kaefean1@gmail.com>
2025-01-02 18:59:02 +0300

    order info bottom sheet

--- sapiensshifter/lib/main.dart
+++ sapiensshifter/lib/main.dart
@@ -4,8 +4,14 @@ import 'package:sapiensshifter/product/utils/ui/dashed_divider.dart';
 import 'package:sapiensshifter/product/utils/ui/dashed_rounded_shape_border.dart';
 
 import 'package:sapiensshifter/feature/theme/appliaction_theme.dart';
+<<<<<<< Updated upstream
 import 'package:sapiensshifter/product/component/preview_order_card.dart';
+=======
+import 'package:sapiensshifter/product/models/order_model.dart';
+import 'package:sapiensshifter/product/utils/dialogs_and_bottom_sheet/order_info_bottom_sheet.dart';
+>>>>>>> Stashed changes
 import 'package:sapiensshifter/product/utils/export_dependency_package/component_export_package.dart';
+import 'package:sapiensshifter/product/utils/export_dependency_package/table_export.dart';
 
 void main() {
   runApp(const MyApp());
@@ -30,6 +36,7 @@ class Thix extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+<<<<<<< Updated upstream
     return Scaffold(
       body: Center(
         child: Column(
@@ -50,6 +57,46 @@ class Thix extends StatelessWidget {
           ],
         ),
       ),
+=======
+    return const Scaffold(
+      body: Center(child: Blabla()),
+    );
+  }
+}
+
+class Blabla extends StatelessWidget {
+  const Blabla({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      child: const Text('a'),
+      onPressed: () {
+        // ignore: inference_failure_on_function_invocation
+        showModalBottomSheet<Widget>(
+          context: context,
+          builder: (BuildContext context) {
+            return OrderInfoBottomSheet(
+              tableModel: TableModel(
+                tableName: 'O3',
+                orderList: [
+                  OrderModel(
+                    orderName: 'Cortado',
+                    imagePath: 'https://coffee.alexflipnote.dev/random',
+                    price: 123,
+                  ),
+                  OrderModel(
+                    orderName: 'Caffee Latte',
+                    imagePath: 'https://coffee.alexflipnote.dev/random',
+                    price: 200,
+                  ),
+                ],
+              ),
+            );
+          },
+        );
+      },
+>>>>>>> Stashed changes
     );
   }
 }
