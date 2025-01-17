36c9c8e3a38328b7ff6a652b5bab6fda3f4870fc
Kaan EFE <kaefean1@gmail.com>
2025-01-17 18:41:32 +0300

    equatable and  json_serializable add all class

---
+++ sapiensshifter/test/extra_model_test.dart
@@ -0,0 +1,21 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:sapiensshifter/product/models/extras_model.dart';
+
+void main() {
+  test(
+    'allOptionsMapEntry Function',
+    () {
+      final milkOptions = ExtrasModel(
+        id: '1',
+        name: 'Milk Options',
+        optionsList: {
+          0.00: const ['Regular Milk', 'Lactose Free'],
+          17.99: const ['Oat Milk', 'Almond Milk', 'Coconut Milk'],
+        },
+      );
+      final result = milkOptions.allOptionsMapEntry();
+      // ignore: avoid_print
+      print(result);
+    },
+  );
+}
