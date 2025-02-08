64591790e6194219e4a9f231339c84469c3c8e28
Kaan EFE <kaefean1@gmail.com>
2025-02-09 00:35:40 +0300

    module_core add

---
+++ sapiensshifter/module/core/lib/src/interfaces/manager_interface/auth_manager_interface.dart
@@ -0,0 +1,48 @@
+import 'package:core/src/models/custom_credential_model.dart';
+
+/// This abstract class defines the interface for an authentication manager.
+/// It contains methods for handling authentication operations such as
+/// signing in, registering, signing in with custom credentials, and signing out.
+abstract class AuthManagerInterface {
+  /// Signs in a user using their email and password.
+  ///
+  /// Parameters:
+  /// - [email]: The email address of the user.
+  /// - [password]: The password of the user.
+  ///
+  /// Returns:
+  /// - A [Future] that resolves to `true` if the sign-in was successful, otherwise `false`.
+  Future<bool> signInWithEmailAndPassword({
+    required String email,
+    required String password,
+  });
+
+  /// Registers a new user using their email and password.
+  ///
+  /// Parameters:
+  /// - [email]: The email address of the user.
+  /// - [password]: The password of the user.
+  ///
+  /// Returns:
+  /// - A [Future] that resolves to `true` if the registration was successful, otherwise `false`.
+  Future<bool> registerInWithEmailAndPassword({
+    required String email,
+    required String password,
+  });
+
+  /// Signs in a user using a custom credential.
+  ///
+  /// Parameters:
+  /// - [credential]: A [CustomCredential] object containing the user's credentials.
+  ///   This can be used for authentication with third-party providers.
+  ///
+  /// Returns:
+  /// - A [Future] that resolves to `true` if the sign-in was successful, otherwise `false`.
+  Future<bool> signInWithCredential({required CustomCredential credential});
+
+  /// Signs out the currently authenticated user.
+  ///
+  /// Returns:
+  /// - A [Future] that resolves to `true` if the sign-out was successful, otherwise `false`.
+  Future<bool> signOut();
+}
---
+++ sapiensshifter/module/core/lib/src/interfaces/operation_interface/auth_operation_interface.dart
@@ -0,0 +1,39 @@
+import 'package:core/src/models/user_model.dart';
+
+/// An abstract interface for performing various authentication operations on a user's profile.
+/// This interface includes methods for updating the user's password, display name,
+/// and profile photograph, as well as a getter to access the current user model.
+abstract class AuthOperationInterface {
+  /// Updates the user's password.
+  ///
+  /// Parameters:
+  /// - [newPassword]: The new password to be set for the user.
+  ///
+  /// Returns:
+  /// A [Future] that resolves to `true` if the password update is successful, otherwise `false`.
+  Future<bool> passwordUpdate(String newPassword);
+
+  /// Updates the user's display name.
+  ///
+  /// Parameters:
+  /// - [newName]: The new display name to be set for the user.
+  ///
+  /// Returns:
+  /// A [Future] that resolves to `true` if the display name update is successful, otherwise `false`.
+  Future<bool> displayUpdate(String newName);
+
+  /// Updates the user's profile photograph URL.
+  ///
+  /// Parameters:
+  /// - [newPhotoUrl]: The new URL of the user's profile photograph.
+  ///
+  /// Returns:
+  /// A [Future] that resolves to `true` if the photograph update is successful, otherwise `false`.
+  Future<bool> photographUpdate(String newPhotoUrl);
+
+  /// Retrieves the current user model.
+  ///
+  /// Returns:
+  /// A [UserModel] representing the current user.
+  UserModel get user;
+}
