82c5f049a1a994362b34a8d09251bdc70f10f546
Kaan EFE <kaefean1@gmail.com>
2025-02-09 00:35:40 +0300

    module_core add

---
+++ sapiensshifter/module/core/lib/src/interfaces/manager_interface/manager_interface.dart
@@ -0,0 +1,2 @@
+export 'auth_manager_interface.dart';
+export 'local_cache_manager_interface.dart';
---
+++ sapiensshifter/module/core/lib/src/interfaces/operation_interface/operation_interface.dart
@@ -0,0 +1,3 @@
+export 'auth_operation_interface.dart';
+export 'local_cache_operation_interface.dart';
+export 'network_store_operation_interface.dart';
