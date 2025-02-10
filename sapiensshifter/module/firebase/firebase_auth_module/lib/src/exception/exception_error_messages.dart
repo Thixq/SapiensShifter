35e1b10934a2a7a7721583e983ad5f380be38b5f
Kaan EFE <kaefean1@gmail.com>
2025-02-10 18:25:35 +0300

    fix

--- sapiensshifter/module/firebase/firebase_auth_module/lib/src/exception/exception_error_messages.dart
+++ sapiensshifter/module/firebase/firebase_auth_module/lib/src/exception/exception_error_messages.dart
@@ -1,21 +1,21 @@
 class ExceptionErrorMessages {
   // Private constructor: Bu sınıfın örneği oluşturulamaz.
-  ExceptionErrorMessages(this.a);
-  late final String a;
+  ExceptionErrorMessages._();
+
   // Hata kodları ile ilgili mesajların saklandığı map.
   static final Map<String, String> _errorMessages = {
-    'invalid-email': a,
-    'user-disabled': 'Kullanıcı engellendi.',
-    'user-not-found': 'Kullanıcı bulunamadı.',
-    'wrong-password': 'Yanlış şifre.',
-    'email-already-in-use': 'E-posta zaten kullanımda.',
-    'weak-password': 'Zayıf şifre.',
-    'operation-not-allowed': 'İşlem izin verilmiyor.',
-    'too-many-requests': 'Çok fazla istek yapıldı.',
-    'requires-recent-login': 'Son giriş gereklidir.',
-    'network-request-failed': 'Ağ isteği başarısız oldu.',
-    'user-token-expired': 'Kullanıcı token süresi doldu.',
-    'invalid-photo-url': 'Geçersiz fotoğraf URL\'si.',
+    'invalid_email': 'Geçersiz e-posta adresi.',
+    'user_disabled': 'Kullanıcı engellendi.',
+    'user_not_found': 'Kullanıcı bulunamadı.',
+    'wrong_password': 'Yanlış şifre.',
+    'email_already_in_use': 'E-posta zaten kullanımda.',
+    'weak_password': 'Zayıf şifre.',
+    'operation_not_allowed': 'İşlem izin verilmiyor.',
+    'too_many_requests': 'Çok fazla istek yapıldı.',
+    'requires_recent_login': 'Son giriş gereklidir.',
+    'network_request_failed': 'Ağ isteği başarısız oldu.',
+    'user_token_expired': 'Kullanıcı token süresi doldu.',
+    'invalid_photo_url': 'Geçersiz fotoğraf URL\'si.',
   };
 
   /// Verilen hata koduna karşılık gelen hata mesajını döner.
