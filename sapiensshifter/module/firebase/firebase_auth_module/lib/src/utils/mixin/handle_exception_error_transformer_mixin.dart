82cc5a95b2c25c6a8971951b5fca98e61d5d400b
Kaan EFE <kaefean1@gmail.com>
2025-02-14 03:15:34 +0300

    firebase_firestore add

--- sapiensshifter/module/firebase/firebase_auth_module/lib/src/utils/mixin/handle_exception_error_transformer_mixin.dart
+++ sapiensshifter/module/firebase/firebase_auth_module/lib/src/utils/mixin/handle_exception_error_transformer_mixin.dart
@@ -1,13 +1,19 @@
+// utils/mixin/handle_exception_error_transformer_mixin.dart
+
 import 'package:core/core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-
 import '../../exception/exception.dart';
+
+part of '../firebase_auth_manager.dart';
 
 mixin HandleExceptionErrorTransformerMixin {
   BaseExceptionInterface handleFirebaseAuthException(
-      FirebaseAuthException exception,
-      [StackTrace? stackTrace]) {
+    FirebaseAuthException exception, [
+    StackTrace? stackTrace,
+  ]) {
     return ModuleFirebaseAuthException(
-        code: exception.code, stackTrace: stackTrace);
+      code: exception.code,
+      stackTrace: stackTrace,
+    );
   }
 }
---
+++ sapiensshifter/module/firebase/firebase_firestore_module/lib/firebase_firestore_module.dart
@@ -0,0 +1,7 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library;
+
+export 'src/firebase_firestore_custom_query.dart';
+export 'src/firebase_firestore_operation.dart';
