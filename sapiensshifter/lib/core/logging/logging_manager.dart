da910fdb97eaf2195a41435306b484230bc4a15b
Kaan EFE <kaefean1@gmail.com>
2025-02-21 22:21:39 +0300

    custom logger documentation added

--- sapiensshifter/lib/core/logging/logging_manager.dart
+++ sapiensshifter/lib/core/logging/logging_manager.dart
@@ -1,20 +1,35 @@
 import 'package:flutter/foundation.dart';
 import 'package:logging/logging.dart';
 
+/// LoggingManager is responsible for initializing and configuring the logging settings.
 class LoggingManager {
-  static void init({Level? level}) {
-    final logLevel = level ?? (kReleaseMode ? Level.WARNING : Level.ALL);
-    Logger.root.level = logLevel;
-    String? logMessage;
+  // Declare a variable to hold the formatted log message.
+  static String? _logMessage;
 
+  /// Initializes logging with an optional [level]. If no level is provided, it sets the log level based on the build mode.
+  static void init({Level? level}) {
+    // Determine the logging level:
+    // - Use the provided level if available.
+    // - If not provided, use Level.WARNING in release mode, otherwise Level.ALL (to log all messages in debug mode).
+    final logLevel = level ?? (kReleaseMode ? Level.WARNING : Level.ALL);
+
+    // Set the root logger's level to control which log messages are processed.
+    Logger.root.level = logLevel;
+
+    // Listen to log records from the root logger.
     Logger.root.onRecord.listen((record) {
-      logMessage =
+      // Format the log message with level, time, logger name, message, and stack trace (if available).
+      _logMessage =
           '${record.level.name} -- ${record.time} -- ${record.loggerName}: ${record.message} \n ${record.stackTrace ?? 'Stack Trace is Null'}';
-      debugPrint(logMessage);
+      // Print the formatted log message to the console.
+      debugPrint(_logMessage);
     });
+
+    // Override Flutter's default error handling to print Flutter errors to the console.
     FlutterError.onError = (FlutterErrorDetails details) {
-      // Hataları konsola yaz
-      debugPrint(logMessage);
+      // Log Flutter error details to the console.
+      // Note: This currently prints the last logMessage; you might consider using details.toString() for more specific info.
+      debugPrint(_logMessage);
     };
   }
 }
--- sapiensshifter/lib/feature/splash/view/splash_view.dart
+++ sapiensshifter/lib/feature/splash/view/splash_view.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:sapiensshifter/core/exception_handler/async_exception_handler.dart';
 import 'package:sapiensshifter/core/logging/custom_logger.dart';
 import 'package:sapiensshifter/product/utils/export_dependency_package/component_export_package.dart';
 import 'package:sapiensshifter/product/utils/ui/svg_asset_builder.dart';
@@ -13,27 +12,6 @@ class SplashView extends StatelessWidget {
   Widget build(BuildContext context) {
     _logger.info('Splash start');
     return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        onPressed: () async {
-          await asyncHandler(
-            context: context,
-            () async {
-              await Future.delayed(
-                const Duration(seconds: 2),
-                () {
-                  print(a);
-                },
-              );
-            },
-            onError: (error, [stackTrace]) {
-              _logger.error(
-                error.toString(),
-                stackTrace: stackTrace,
-              );
-            },
-          );
-        },
-      ),
       body: Center(
         child: SvgAssetBuilder(
           svgPath: 'assets/logo/coffee_sapiens_logo.svg',
