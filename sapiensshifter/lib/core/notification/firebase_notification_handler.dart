2d93164db560b6f7596f822b5ef8dc4de5b3850f
Kaan EFE <kaefean1@gmail.com>
2025-03-12 22:37:43 +0300

    fix

--- sapiensshifter/lib/core/notification/firebase_notification_handler.dart
+++ sapiensshifter/lib/core/notification/firebase_notification_handler.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: unused_element
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
