6f6ccc6c1b1976e96a8f1dc492eaf47b7b01980b
Kaan EFE <kaefean1@gmail.com>
2025-03-19 22:03:16 +0300

    splash view done

--- sapiensshifter/lib/core/routing/routing_manager.gr.dart
+++ sapiensshifter/lib/core/routing/routing_manager.gr.dart
@@ -9,21 +9,39 @@
 // coverage:ignore-file
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
-import 'package:auto_route/auto_route.dart' as _i2;
-import 'package:sapiensshifter/feature/splash/view/splash_view.dart' as _i1;
+import 'package:auto_route/auto_route.dart' as _i3;
+import 'package:sapiensshifter/feature/sign/sign_in/view/sign_in_view.dart'
+    as _i1;
+import 'package:sapiensshifter/feature/splash/view/splash_view.dart' as _i2;
 
 /// generated route for
-/// [_i1.SplashView]
-class SplashRoute extends _i2.PageRouteInfo<void> {
-  const SplashRoute({List<_i2.PageRouteInfo>? children})
+/// [_i1.SignInView]
+class SignInRoute extends _i3.PageRouteInfo<void> {
+  const SignInRoute({List<_i3.PageRouteInfo>? children})
+    : super(SignInRoute.name, initialChildren: children);
+
+  static const String name = 'SignInRoute';
+
+  static _i3.PageInfo page = _i3.PageInfo(
+    name,
+    builder: (data) {
+      return const _i1.SignInView();
+    },
+  );
+}
+
+/// generated route for
+/// [_i2.SplashView]
+class SplashRoute extends _i3.PageRouteInfo<void> {
+  const SplashRoute({List<_i3.PageRouteInfo>? children})
     : super(SplashRoute.name, initialChildren: children);
 
   static const String name = 'SplashRoute';
 
-  static _i2.PageInfo page = _i2.PageInfo(
+  static _i3.PageInfo page = _i3.PageInfo(
     name,
     builder: (data) {
-      return const _i1.SplashView();
+      return const _i2.SplashView();
     },
   );
 }
---
+++ sapiensshifter/lib/feature/splash/view_model/enum/splash_state_enum.dart
@@ -0,0 +1,3 @@
+// ignore_for_file:constant_identifier_names
+
+enum SplashStateEnum { NO_NETWORK, FIRST_LAUNCH, RETURNIG_USER, NEW_USER }
