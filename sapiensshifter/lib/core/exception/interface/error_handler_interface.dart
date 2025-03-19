81d7c354a115fd09823f890765980f44dfbdf187
Kaan EFE <kaefean1@gmail.com>
2025-03-19 16:22:49 +0300

    error handler add docs

--- sapiensshifter/lib/core/exception/interface/error_handler_interface.dart
+++ sapiensshifter/lib/core/exception/interface/error_handler_interface.dart
@@ -1,5 +1,6 @@
 import 'package:sapiensshifter/core/logging/custom_logger.dart';
 
+/// Skeleton class for error handling
 abstract class IErrorHandler {
   void showError(Object error);
   void handleError(Object error, CustomLogger logger, StackTrace stackTrace);
--- sapiensshifter/lib/core/utils/mixin/exception/show_excepiton_dialog/show_exception_dialog.dart
+++ sapiensshifter/lib/core/utils/mixin/exception/show_excepiton_dialog/show_exception_dialog.dart
@@ -1,14 +1,13 @@
-// ignore_for_file: unused_element, inference_failure_on_function_invocation
-
 import 'package:flutter/material.dart';
 
+///
 mixin ShowExceptionDialogMixin {
   Future<void> showErrorDialog(
     BuildContext context, {
     required String desc,
     String? title,
-  }) async {
-    await showAdaptiveDialog(
+  }) {
+    return showAdaptiveDialog(
       context: context,
       builder: (context) => AlertDialog.adaptive(
         title: Text(title ?? 'Error'),
