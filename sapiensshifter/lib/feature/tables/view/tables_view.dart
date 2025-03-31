9754aaa99c93da0b1be40d9ea4299ad619c1bddf
Kaan EFE <kaefean1@gmail.com>
2025-03-31 23:20:30 +0300

    firestore path checker fixed

--- sapiensshifter/lib/feature/tables/view/tables_view.dart
+++ sapiensshifter/lib/feature/tables/view/tables_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:sapiensshifter/core/state/base/base_state.dart';
+import 'package:sapiensshifter/feature/tables/mixin/tables_view_mixin.dart';
 import 'package:sapiensshifter/product/models/order_model.dart';
 import 'package:sapiensshifter/product/models/table_model.dart';
 import 'package:sapiensshifter/product/utils/export_dependency_package/component.dart';
@@ -14,16 +16,27 @@ class TablesView extends StatefulWidget {
   State<TablesView> createState() => _TablesViewState();
 }
 
-class _TablesViewState extends State<TablesView> {
+class _TablesViewState extends BaseState<TablesView> with TablesViewMixin {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: const TablesViewAppBar(
         height: 1,
       ),
-      body: Padding(
-        padding: EdgeInsets.all(context.sized.normalValue),
-        child: const TableGrid(),
+      body: viewModel.getTableList.ext.toBuild(
+        onSuccess: (data) => Padding(
+          padding: EdgeInsets.all(context.sized.normalValue),
+          child: TableGrid(
+            tableList: data ?? [],
+          ),
+        ),
+        loadingWidget: const CircularProgressIndicator(),
+        notFoundWidget: Container(
+          color: Colors.amber,
+        ),
+        onError: Container(
+          color: Colors.red,
+        ),
       ),
     );
   }
