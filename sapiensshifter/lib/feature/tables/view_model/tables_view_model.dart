d3ab53c02fde4f954a522c2c961655e4c9301058
Kaan EFE <kaefean1@gmail.com>
2025-04-22 22:31:59 +0300

    tablesviewmodel getProduct path fix

--- sapiensshifter/lib/feature/tables/view_model/tables_view_model.dart
+++ sapiensshifter/lib/feature/tables/view_model/tables_view_model.dart
@@ -4,6 +4,7 @@ import 'package:sapiensshifter/core/exception/utils/error_util.dart';
 import 'package:sapiensshifter/core/logging/custom_logger.dart';
 import 'package:sapiensshifter/core/state/base/base_cubit.dart';
 import 'package:sapiensshifter/feature/tables/view_model/state/tables_view_state.dart';
+import 'package:sapiensshifter/product/constant/query_path_constant.dart';
 import 'package:sapiensshifter/product/models/table_model/table_model.dart';
 import 'package:sapiensshifter/product/profile/profile.dart';
 
@@ -25,8 +26,9 @@ class TablesViewModel extends BaseCubit<TablesViewState> {
     );
     final result = await ErrorUtil.runWithErrorHandlingAsync<List<TableModel>>(
       action: () async {
+        final branchId = await profile.getToDayBranchId;
         return _networkManager.networkOperation.getItemsQuery(
-          path: 'table/kanyon/open',
+          path: '${QueryPathConstant.tableColPath}/$branchId/open',
           model: const TableModel(),
         );
       },
