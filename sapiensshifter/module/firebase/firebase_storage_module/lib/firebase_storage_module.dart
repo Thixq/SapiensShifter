7e2f0a74356909916b57c14992f4a9ec9b97fe20
Kaan EFE <kaefean1@gmail.com>
2025-05-28 23:00:09 +0300

    libary add

--- sapiensshifter/module/firebase/firebase_storage_module/lib/firebase_storage_module.dart
+++ sapiensshifter/module/firebase/firebase_storage_module/lib/firebase_storage_module.dart
@@ -1,8 +1,3 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
 library;
 
-export 'src/firebase_storage_module_base.dart';
-
-// TODO: Export any libraries intended for clients of this package.
+export 'src/firebase_storage_module.dart';
