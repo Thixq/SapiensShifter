77f90fb6f4920d43c35e2aed19b409eb11800319
Kaan EFE <kaefean1@gmail.com>
2025-06-18 20:30:18 +0300

    Create shift_add_content.dart

---
+++ sapiensshifter/lib/feature/shift_add/view/widget/shift_add_content.dart
@@ -0,0 +1,77 @@
+part of '../shift_add_view.dart';
+
+final _weelLocalization = <String>[
+  LocaleKeys.date_week_days_monday.tr(),
+  LocaleKeys.date_week_days_tuesday.tr(),
+  LocaleKeys.date_week_days_wednesday.tr(),
+  LocaleKeys.date_week_days_thursday.tr(),
+  LocaleKeys.date_week_days_friday.tr(),
+  LocaleKeys.date_week_days_saturday.tr(),
+  LocaleKeys.date_week_days_sunday.tr(),
+];
+
+class ShiftAddContent extends StatelessWidget {
+  ShiftAddContent({
+    required this.formKey,
+    required this.onWeekPeriod,
+    required this.peopleMap,
+    required this.onConfrim,
+    required this.onPeople,
+    required this.onShiftDay,
+    required this.branchs,
+    required this.shifts,
+    super.key,
+  }) : _week = List.generate(
+          7,
+          (index) => BranchAndShift(
+            title: _weelLocalization[index],
+            index: index,
+            onShiftDay: onShiftDay,
+            branch: branchs,
+            shift: shifts,
+          ),
+        );
+
+  final GlobalKey<FormState> formKey;
+  final void Function(WeekPeriod weekPeriod) onWeekPeriod;
+  final void Function(String? peopleId) onPeople;
+  final Map<String, String> peopleMap;
+  final VoidCallback onConfrim;
+  final void Function(ShiftDay shiftDay, int index) onShiftDay;
+  final Map<String, String> branchs;
+  final Map<String, String> shifts;
+  final List<BranchAndShift> _week;
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: SingleChildScrollView(
+        child: Form(
+          key: formKey,
+          child: SeparatorListWidget(
+            separator: context.sized.emptySizedHeightBoxLow,
+            children: [
+              WeeklyPeriod(
+                onChange: onWeekPeriod,
+              ),
+              SapiCustomDropDown<String>(
+                validator: GenericValidator.emptyValidator,
+                hintText: LocaleKeys.page_sihft_add_view_choice_people.tr(),
+                items: peopleMap,
+                onSelected: onPeople,
+              ),
+              ..._week,
+              SizedBox(
+                width: double.infinity,
+                child: SapiButton(
+                  buttonText: LocaleKeys.confirm.tr(),
+                  onPressed: onConfrim,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
