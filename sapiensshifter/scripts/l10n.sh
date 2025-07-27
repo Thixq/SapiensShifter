a820b6aff7f39e9c47004056c7400baac71edd6f
Kaan EFE <kaefean1@gmail.com>
2025-07-27 15:50:45 +0300

    ios build script

--- sapiensshifter/scripts/l10n.sh
+++ sapiensshifter/scripts/l10n.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 flutter pub run easy_localization:generate -S assets/translations -O lib/core/localization/localization_codegen
 flutter pub run easy_localization:generate -S assets/translations -O lib/core/localization/localization_codegen -f keys -o locale_keys.g.dart
 
